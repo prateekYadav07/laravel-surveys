cc83d8c27814fd3e853a629727fd68d3a640f159
prateekYadav07 <yadavprateek39@gmail.com>
2023-02-21 22:41:03 +0530

    app-layout changes

--- vue/src/components/app-layout.vue
+++ vue/src/components/app-layout.vue
@@ -1,84 +1,86 @@
 <template>
-    <v-app id="inspire">
-        <v-app-bar>
-            <v-toolbar-title class="me-10">
-                <v-avatar
-                    class="me-5 ms-4"
-                    color="grey-darken-1"
-                    size="32"
-                ></v-avatar>
-                Surveys
-            </v-toolbar-title>
-            <template v-if="module != 'login'">
-                <v-btn
-                    v-for="link in links"
-                    :key="link"
-                    variant="text"
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="fill-height d-flex align-center">
+        <v-avatar
+          class="me-10 ms-4"
+          color="grey-darken-1"
+          size="32"
+        ></v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          variant="text"
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            density="compact"
+            hide-details
+            variant="solo"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
                 >
-                    {{ link }}
-                </v-btn>
+                  <v-list-item-title>
+                    List Item {{ n }}
+                  </v-list-item-title>
+                </v-list-item>
 
-                <v-spacer></v-spacer>
+                <v-divider class="my-2"></v-divider>
 
-                <v-responsive max-width="260">
-                    <v-text-field
-                        density="compact"
-                        hide-details
-                        variant="solo"
-                    ></v-text-field>
-                </v-responsive>
-            </template>
-        </v-app-bar>
+                <v-list-item
+                  link
+                  color="grey-lighten-4"
+                >
+                  <v-list-item-title>
+                    Refresh
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
 
-        <v-main >
-            <template v-slot:main>
-
-            </template>
-        </v-main>
-<!--        <v-main class="bg-grey-lighten-2">-->
-<!--            <v-container>-->
-<!--                <v-row>-->
-<!--                    <template v-for="n in 4" :key="n">-->
-<!--                        <v-col-->
-<!--                            class="mt-2"-->
-<!--                            cols="12"-->
-<!--                        >-->
-<!--                            <strong>Category {{ n }}</strong>-->
-<!--                        </v-col>-->
-
-<!--                        <v-col-->
-<!--                            v-for="j in 6"-->
-<!--                            :key="`${n}${j}`"-->
-<!--                            cols="6"-->
-<!--                            md="2"-->
-<!--                        >-->
-<!--                            <v-sheet height="150"></v-sheet>-->
-<!--                        </v-col>-->
-<!--                    </template>-->
-<!--                </v-row>-->
-<!--            </v-container>-->
-<!--        </v-main>-->
-    </v-app>
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-    export default {
-        props:{
-            module: {
-                type: String,
-                required: true
-            }
-        },
-        data() {
-            return {
-                drawer: null,
-                links: [
-                    'Dashboard',
-                    'Messages',
-                    'Profile',
-                    'Updates',
-                ],
-            }
-        }
-    }
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
+  }
 </script>
